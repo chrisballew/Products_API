ea7e51c741d7529a97b22214b7fac00bb739751c
Chris <bluman325@gmail.com>
2022-03-31 15:17:03 -0500

    created products app

--- product_project/settings.py
+++ product_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@6zodk*b2v!t=bag89e=bbhbfw%ah093#c=x5r_hjqv8+_lf2*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -109,3 +106,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from product_project.local_settings import *
+except ImportError:
+    pass
