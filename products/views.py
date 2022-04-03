9b4da72c4b81e12ca049142c54d0f7d6819499f4
Chris <bluman325@gmail.com>
2022-04-03 14:27:47 -0500

    space out code for readability in views file in products app folder

--- products/views.py
+++ products/views.py
@@ -8,27 +8,35 @@ from products import serializers
 
 @api_view(['GET', 'POST'])
 def products_list(request):
+
     if request.method == 'GET':
         products = Product.objects.all()   
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
+
     if request.method == 'GET':
         serializer = ProductSerializer(product);
         return Response(serializer.data)
+
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
